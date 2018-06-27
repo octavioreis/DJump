44b47f0c3657a135372adb67aad2df1fb402f793
octavioreis <octavioreis@users.noreply.github.com>
2018-06-27 17:23:03 -0300

    return to menu from score screen and refactors

--- DJump/Assets/Scripts/HighScoresManager.cs
+++ DJump/Assets/Scripts/HighScoresManager.cs
@@ -1,31 +1,12 @@
 ﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.UI;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public class HighScoresManager : MonoBehaviour
 {
-    public GameObject _playerScoreEntryPrefab;
+    public string MainMenuSceneName;
 
-    void Start()
+    public void ReturnToMainMenu()
     {
-        var scores = new List<PlayerScore>()
-        {
-            new PlayerScore("TESTE1", 30),
-            new PlayerScore("TESTE2", 20),
-            new PlayerScore("TESTE3", 10)
-        };
-
-        foreach (var playerScore in scores)
-        {
-            var playerScoreEntry = Instantiate(_playerScoreEntryPrefab);
-            playerScoreEntry.transform.SetParent(transform);
-
-            var playerNameText = playerScoreEntry.transform.Find("PlayerNameText").GetComponent<Text>();
-            playerNameText.text = playerScore.Name;
-
-            var playerScoreText = playerScoreEntry.transform.Find("PlayerScoreText").GetComponent<Text>();
-            playerScoreText.text = playerScore.Score.ToString();
-        }
+        SceneManager.LoadScene(MainMenuSceneName);
     }
 }
---
+++ DJump/Assets/Scripts/PlayerScoreListManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScoreListManager : MonoBehaviour
+{
+    public GameObject _playerScoreEntryPrefab;
+
+    void Start()
+    {
+        //var scores = new List<PlayerScore>()
+        //{
+        //    new PlayerScore("TESTE1", 30),
+        //    new PlayerScore("TESTE2", 20),
+        //    new PlayerScore("TESTE3", 10)
+        //};
+
+        var scores = SaveManager.Instance.PlayerScores;
+
+        foreach (var playerScore in scores)
+        {
+            var playerScoreEntry = Instantiate(_playerScoreEntryPrefab);
+            playerScoreEntry.transform.SetParent(transform);
+
+            var playerNameText = playerScoreEntry.transform.Find("PlayerNameText").GetComponent<Text>();
+            playerNameText.text = playerScore.Name;
+
+            var playerScoreText = playerScoreEntry.transform.Find("PlayerScoreText").GetComponent<Text>();
+            playerScoreText.text = playerScore.Score.ToString();
+        }
+    }
+}
